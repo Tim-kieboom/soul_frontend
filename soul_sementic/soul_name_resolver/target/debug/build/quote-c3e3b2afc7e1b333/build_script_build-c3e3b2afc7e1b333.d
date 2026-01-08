97c6a7667f20acc6176efce4bfc8f6930ad21fee
tim kieboom <113353374+Tim-kieboom@users.noreply.github.com>
2026-01-08 22:53:21 +0100

    remove /target

--- soul_sementic/soul_name_resolver/target/debug/build/quote-c3e3b2afc7e1b333/build_script_build-c3e3b2afc7e1b333.d
+++
@@ -1,5 +0,0 @@
-F:\Code\Github\soul_frontend\soul_sementic\soul_name_resolver\target\debug\build\quote-c3e3b2afc7e1b333\build_script_build-c3e3b2afc7e1b333.d: C:\Users\tim_k\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.43\build.rs
-
-F:\Code\Github\soul_frontend\soul_sementic\soul_name_resolver\target\debug\build\quote-c3e3b2afc7e1b333\build_script_build-c3e3b2afc7e1b333.exe: C:\Users\tim_k\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.43\build.rs
-
-C:\Users\tim_k\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.43\build.rs:
--- soul_sementic/soul_name_resolver/target/debug/deps/soul_name_resolver-8cdda8484d4a3c75.d
+++
@@ -1,5 +0,0 @@
-F:\Code\Github\soul_frontend\soul_sementic\soul_name_resolver\target\debug\deps\soul_name_resolver-8cdda8484d4a3c75.d: src\main.rs
-
-F:\Code\Github\soul_frontend\soul_sementic\soul_name_resolver\target\debug\deps\libsoul_name_resolver-8cdda8484d4a3c75.rmeta: src\main.rs
-
-src\main.rs:
