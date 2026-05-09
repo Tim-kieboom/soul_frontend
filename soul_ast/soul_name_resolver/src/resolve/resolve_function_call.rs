285c12d5900ea29b211afe2d848c30ab07fffeeb
tim kieboom <113353374+Tim-kieboom@users.noreply.github.com>
2026-05-09 22:10:35 +0200

    added intrinsic fake module for intrinsic fn's

--- soul_ast/soul_name_resolver/src/resolve/resolve_function_call.rs
+++ soul_ast/soul_name_resolver/src/resolve/resolve_function_call.rs
@@ -74,26 +74,29 @@ impl<'a> NameResolver<'a> {
     }
 
     fn get_resolve(&mut self, function_call: &mut FunctionCall) {
-        if function_call.callee.is_none() {
-            if let Some(intrinsic) = ast::Intrinsic::from_name(function_call.name.as_str()) {
-                function_call.intrinsic = Some(intrinsic);
-                function_call.resolved = Some(FunctionId::error());
-
-                if intrinsic == ast::Intrinsic::InFile && !self.current.resolving_default {
-                    let file_path = self.current.current_path();
-                    function_call.intrinsic_value = Some(ast::IntrinsicValue::Path(file_path));
-                } else if intrinsic == ast::Intrinsic::InLine && !self.current.resolving_default {
-                    let line = function_call.name.span.start_line as i128;
-                    function_call.intrinsic_value = Some(ast::IntrinsicValue::Literal(ast::Literal::Int(line)));
-                }
-                return;
-            }
-        }
-
         let callee_ident = match function_call.callee.as_ref().map(|f| &f.node) {
             Some(ExpressionKind::Variable { ident, .. }) => Some(ident.to_string()),
             _ => None,
         };
+
+        if let Some(callee_name) = &callee_ident {
+            if callee_name == "intrinsic" {
+                if let Some(intrinsic) = ast::Intrinsic::from_name(function_call.name.as_str()) {
+                    function_call.intrinsic = Some(intrinsic);
+                    function_call.resolved = Some(FunctionId::error());
+                    function_call.callee = None;
+
+                    if intrinsic == ast::Intrinsic::InFile && !self.current.resolving_default {
+                        let file_path = self.current.current_path();
+                        function_call.intrinsic_value = Some(ast::IntrinsicValue::Path(file_path));
+                    } else if intrinsic == ast::Intrinsic::InLine && !self.current.resolving_default {
+                        let line = function_call.name.span.start_line as i128;
+                        function_call.intrinsic_value = Some(ast::IntrinsicValue::Literal(ast::Literal::Int(line)));
+                    }
+                    return;
+                }
+            }
+        }
 
         let module_entry = callee_ident
             .as_ref()
